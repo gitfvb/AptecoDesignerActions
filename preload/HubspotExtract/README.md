5bd4ea79cbaaf9ac56833d8ba7cbc7a77597ac14
FlorianSf <florian.von.bracht@apteco.de>
2021-02-02 16:51:34 +0100

    Added notes to handle the HAPIKEY much better

--- preload/HubspotExtract/README.md
+++ preload/HubspotExtract/README.md
@@ -1,1 +1,5 @@
-Replace <hapikey> with your Hubspot API Key
+# Setup
+
+* Put in your token into the environment variable `HAPIKEY`. It will be stored encrypted.<br/><br/>![grafik](https://user-images.githubusercontent.com/14135678/106619941-624f3500-6571-11eb-9452-276dee404f8e.png)
+
+* Create a pre load action that saves the current token encrypted into the scriptPath:<br/><br/>![grafik](https://user-images.githubusercontent.com/14135678/106621293-b575b780-6572-11eb-9b8a-a9b335545e41.png)
---
+++ preload/HubspotExtract/hubspot__01__save_api_key.ps1
@@ -0,0 +1,151 @@
+﻿################################################
+#
+# INPUT
+#
+################################################
+
+Param(
+     $scriptPath
+)
+
+#-----------------------------------------------
+# DEBUG SWITCH
+#-----------------------------------------------
+
+$debug = $false
+
+
+
+
+################################################
+#
+# NOTES
+#
+################################################
+
+
+
+################################################
+#
+# SCRIPT ROOT
+#
+################################################
+
+#if ( $debug ) {
+    # Load scriptpath
+    if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+        $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+    } else {
+        $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+    }
+#} else {
+#    $scriptPath = "$( $params.scriptPath )" 
+#}
+Set-Location -Path $scriptPath
+
+
+################################################
+#
+# SETTINGS
+#
+################################################
+
+# General settings
+$functionsSubfolder = "functions"
+$libSubfolder = "lib"
+$settingsFilename = "settings.json"
+$lastSessionFilename = "lastsession.json"
+$processId = [guid]::NewGuid()
+$modulename = "hubspot_apikey"
+$timestamp = [datetime]::Now
+
+# Load last session
+#$lastSession = Get-Content -Path "$( $scriptPath )\$( $lastSessionFilename )" -Encoding UTF8 -Raw | ConvertFrom-Json
+
+# Load settings
+#$settings = Get-Content -Path "$( $scriptPath )\$( $settingsFilename )" -Encoding UTF8 -Raw | ConvertFrom-Json
+
+# TODO  [ ] unify settings in json file
+$settings = @{
+
+    # Security settings
+    aesFile = "$( $scriptPath )\aes.key"
+
+    logfile = "$( $scriptPath )\hubspot_extract.log"
+
+}
+
+# Log
+$logfile = $settings.logfile
+
+# append a suffix, if in debug mode
+if ( $debug ) {
+    $logfile = "$( $logfile ).debug"
+}
+
+
+################################################
+#
+# FUNCTIONS & LIBRARIES
+#
+################################################
+
+# Load all PowerShell Code
+"Loading..."
+Get-ChildItem -Path ".\$( $functionsSubfolder )" -Recurse -Include @("*.ps1") | ForEach {
+    . $_.FullName
+    "... $( $_.FullName )"
+}
+
+
+
+################################################
+#
+# MORE SETTINGS AFTER LOADING FUNCTIONS
+#
+################################################
+
+
+# Create general settings
+$keyfilename = $settings.aesFile
+$hapikeyFilename = ".\hapi.key" # [ ] TODO Replace with $settings.hapikeyFile 
+$keyFile = $settings.aesFile # [ ] TODO Check if this is needed
+
+
+################################################
+#
+# LOG INPUT PARAMETERS
+#
+################################################
+
+# Start the log
+Write-Log -message "----------------------------------------------------"
+Write-Log -message "$( $modulename )"
+Write-Log -message "Got a file with these arguments: $( [Environment]::GetCommandLineArgs() )"
+
+# Check if params object exists
+if (Get-Variable "params" -Scope Global -ErrorAction SilentlyContinue) {
+    $paramsExisting = $true
+} else {
+    $paramsExisting = $false
+}
+
+# Log the params, if existing
+if ( $paramsExisting ) {
+    $params.Keys | ForEach-Object {
+        $param = $_
+        Write-Log -message "    $( $param ): $( $params[$param] )"
+    }
+}
+
+
+################################################
+#
+# SAVE TOKEN ENCRYPTED IN A SEPARATE FILE
+#
+################################################
+
+Get-PlaintextToSecure -String $Env:HAPIKEY | Set-Content -Path $hapikeyFilename -Encoding UTF8
+
+
+
