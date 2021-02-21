a03f2f000ea04160c1b6d039b002ad415e78ab04
FlorianSf <florian.von.bracht@apteco.de>
2021-02-22 00:03:37 +0100

    First draft for PowerShell emarsys class

---
+++ preload/emarsysExtract/emarsys__00__create_settings.ps1
@@ -0,0 +1,253 @@
+﻿
+################################################
+#
+# START
+#
+################################################
+
+#-----------------------------------------------
+# LOAD SCRIPTPATH
+#-----------------------------------------------
+
+if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+    $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+} else {
+    $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+}
+
+Set-Location -Path $scriptPath
+
+
+#-----------------------------------------------
+# LOAD MORE FUNCTIONS
+#-----------------------------------------------
+
+$functionsSubfolder = ".\functions"
+
+"Loading..."
+Get-ChildItem -Path ".\$( $functionsSubfolder )" -Recurse -Include @("*.ps1") | ForEach {
+    . $_.FullName
+    "... $( $_.FullName )"
+}
+
+
+#-----------------------------------------------
+# ASK FOR SETTINGSFILE
+#-----------------------------------------------
+
+# Default file
+$settingsFileDefault = "$( $scriptPath )\settings.json"
+
+# Ask for another path
+$settingsFile = Read-Host -Prompt "Where do you want the settings file to be saved? Just press Enter for this default [$( $settingsFileDefault )]"
+
+# ALTERNATIVE: The file dialog is not working from Visual Studio Code, but is working from PowerShell ISE or "normal" PowerShell Console
+#$settingsFile = Set-FileName -initialDirectory "$( $scriptPath )" -filter "JSON files (*.json)|*.json"
+
+# If prompt is empty, just use default path
+if ( $settingsFile -eq "" -or $null -eq $settingsFile) {
+    $settingsFile = $settingsFileDefault
+}
+
+# Check if filename is valid
+if(Test-Path -LiteralPath $settingsFile -IsValid ) {
+    Write-Host "SettingsFile '$( $settingsFile )' is valid"
+} else {
+    Write-Host "SettingsFile '$( $settingsFile )' contains invalid characters"
+}
+
+
+#-----------------------------------------------
+# ASK FOR LOGFILE
+#-----------------------------------------------
+
+# Default file
+$logfileDefault = "$( $scriptPath )\emarsys.log"
+
+# Ask for another path
+$logfile = Read-Host -Prompt "Where do you want the log file to be saved? Just press Enter for this default [$( $logfileDefault )]"
+
+# ALTERNATIVE: The file dialog is not working from Visual Studio Code, but is working from PowerShell ISE or "normal" PowerShell Console
+#$settingsFile = Set-FileName -initialDirectory "$( $scriptPath )" -filter "JSON files (*.json)|*.json"
+
+# If prompt is empty, just use default path
+if ( $logfile -eq "" -or $null -eq $logfile) {
+    $logfile = $logfileDefault
+}
+
+# Check if filename is valid
+if(Test-Path -LiteralPath $logfile -IsValid ) {
+    Write-Host "Logfile '$( $logfile )' is valid"
+} else {
+    Write-Host "Logfile '$( $logfile )' contains invalid characters"
+}
+
+
+#-----------------------------------------------
+# ASK FOR UPLOAD FOLDER
+#-----------------------------------------------
+
+# Default file
+$uploadDefault = "$( $scriptPath )\uploads"
+
+# Ask for another path
+$upload = Read-Host -Prompt "Where do you want the files to be uploaded? Just press Enter for this default [$( $uploadDefault )]"
+
+# If prompt is empty, just use default path
+if ( $upload -eq "" -or $null -eq $upload) {
+    $upload = $uploadDefault
+}
+
+# Check if filename is valid
+if(Test-Path -LiteralPath $upload -IsValid ) {
+    Write-Host "Upload folder '$( $upload )' is valid"
+} else {
+    Write-Host "Upload folder '$( $upload )' contains invalid characters"
+}
+
+
+
+#-----------------------------------------------
+# ASK FOR DOWNLOAD FOLDER
+#-----------------------------------------------
+
+# Default file
+$downloadDefault = "$( $scriptPath )\downloads"
+
+# Ask for another path
+$download = Read-Host -Prompt "Where do you want the files to be downloaded? Just press Enter for this default [$( $downloadDefault )]"
+
+# If prompt is empty, just use default path
+if ( $download -eq "" -or $null -eq $download) {
+    $download = $downloadDefault
+}
+
+# Check if filename is valid
+if(Test-Path -LiteralPath $upload -IsValid ) {
+    Write-Host "Upload folder '$( $download )' is valid"
+} else {
+    Write-Host "Upload folder '$( $download )' contains invalid characters"
+}
+
+
+
+################################################
+#
+# SETUP SETTINGS
+#
+################################################
+
+#-----------------------------------------------
+# SECURITY / LOGIN
+#-----------------------------------------------
+
+$keyfile = "$( $scriptPath )\aes.key"
+$user = Read-Host -Prompt "Please enter the username for emarsys"
+$secret = Read-Host -AsSecureString "Please enter the secret for emarsys"
+$secretEncrypted = Get-PlaintextToSecure ((New-Object PSCredential "dummy",$token).GetNetworkCredential().Password)
+
+$loginSettings = @{
+    username = $user
+    secret = $secretEncrypted
+}
+
+ 
+#-----------------------------------------------
+# MAILINGS SETTINGS
+#-----------------------------------------------
+
+$mailingsSettings = @{
+    "languageCode" = "de"   # languagecode for email templates in elettershop
+}
+
+
+#-----------------------------------------------
+# PREVIEW SETTINGS
+#-----------------------------------------------
+
+$previewSettings = @{
+    "Type" = "Email"                # Email|Sms
+    "FromAddress"="info@apteco.de"  # 
+    "FromName"="Apteco"             # 
+    "ReplyTo"="info@apteco.de"      # 
+    "Subject"="Test-Subject"        # 
+}
+
+
+#-----------------------------------------------
+# UPLOAD SETTINGS
+#-----------------------------------------------
+
+$uploadSettings = @{
+    folder = $upload
+}
+
+
+#-----------------------------------------------
+# UPLOAD SETTINGS
+#-----------------------------------------------
+
+$downloadSettings = @{
+    folder = $download
+}
+
+
+
+#-----------------------------------------------
+# ALL SETTINGS
+#-----------------------------------------------
+
+# TODO [ ] use url from PeopleStage Channel Editor Settings instead?
+# TODO [ ] Documentation of all these parameters and the ones above
+
+$settings = @{
+
+    # General
+    base            = "https://trunk-int.s.emarsys.com/api/v2/" #"https://api.emarsys.net/api/v2/"  # Default url
+    changeTLS       = $true                             # should tls be changed on the system?
+    nameConcatChar  = " / "                             # character to concat mailing/campaign id with mailing/campaign name
+    logfile         = $logfile                          # path and name of log file
+    providername    = "emarsys"                         # identifier for this custom integration, this is used for the response allocation
+
+    # Session 
+    aesFile         = $keyFile
+    
+    # Detail settings
+    login = $loginSettings
+    download = $downloadSettings
+    #mailings = $mailingsSettings
+    #preview = $previewSettings
+    upload = $uploadSettings
+
+}
+
+
+################################################
+#
+# PACK TOGETHER SETTINGS AND SAVE AS JSON
+#
+################################################
+
+# create json object
+$json = $settings | ConvertTo-Json -Depth 8 # -compress
+
+# save settings to file
+$json | Set-Content -path "$( $settingsFile )" -Encoding UTF8
+
+
+################################################
+#
+# CREATE FOLDERS IF NEEDED
+#
+################################################
+
+if ( !(Test-Path -Path $settings.upload.folder) ) {
+    Write-Log -message "Upload $( $settings.upload.folder ) does not exist. Creating the folder now!"
+    New-Item -Path "$( $settings.upload.folder )" -ItemType Directory
+}
+
+
+if ( !(Test-Path -Path $settings.download.folder) ) {
+    Write-Log -message "Download $( $settings.download.folder ) does not exist. Creating the folder now!"
+    New-Item -Path "$( $settings.download.folder )" -ItemType Directory
+}
---
+++ preload/emarsysExtract/emarsys__10__getmailings.ps1
@@ -0,0 +1,271 @@
+﻿################################################
+#
+# INPUT
+#
+################################################
+
+Param(
+    [hashtable] $params
+)
+
+#-----------------------------------------------
+# DEBUG SWITCH
+#-----------------------------------------------
+
+$debug = $true
+
+
+#-----------------------------------------------
+# INPUT PARAMETERS, IF DEBUG IS TRUE
+#-----------------------------------------------
+
+if ( $debug ) {
+    $params = [hashtable]@{
+	    Password= "def"
+        scriptPath = "C:\Users\Florian\Documents\GitHub\AptecoDesignerActions\preload\emarsysExtract"
+	    abc= "def"
+	    Username= "abc"
+    }
+}
+
+
+################################################
+#
+# NOTES
+#
+################################################
+
+<#
+
+https://dev.emarsys.com/v2/first-steps/1-prerequisites
+
+#>
+
+
+################################################
+#
+# SCRIPT ROOT
+#
+################################################
+
+if ( $debug ) {
+    # Load scriptpath
+    if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+        $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+    } else {
+        $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+    }
+} else {
+    $scriptPath = "$( $params.scriptPath )" 
+}
+Set-Location -Path $scriptPath
+
+
+################################################
+#
+# SETTINGS
+#
+################################################
+
+# General settings
+$functionsSubfolder = "functions"
+$libSubfolder = "lib"
+$settingsFilename = "settings.json"
+$moduleName = "EMARSYSMAILINGS"
+$processId = [guid]::NewGuid()
+
+# Load settings
+$settings = Get-Content -Path "$( $scriptPath )\$( $settingsFilename )" -Encoding UTF8 -Raw | ConvertFrom-Json
+
+# Allow only newer security protocols
+# hints: https://www.frankysweb.de/powershell-es-konnte-kein-geschuetzter-ssltls-kanal-erstellt-werden/
+if ( $settings.changeTLS ) {
+    $AllProtocols = @(    
+        [System.Net.SecurityProtocolType]::Tls12
+    )
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+}
+
+# more settings
+$logfile = $settings.logfile
+#$guid = ([guid]::NewGuid()).Guid # TODO [ ] use this guid for a specific identifier of this job in the logfiles
+
+# append a suffix, if in debug mode
+if ( $debug ) {
+    $logfile = "$( $logfile ).debug"
+}
+
+
+################################################
+#
+# FUNCTIONS
+#
+################################################
+
+# Load all PowerShell Code
+"Loading..."
+Get-ChildItem -Path ".\$( $functionsSubfolder )" -Recurse -Include @("*.ps1") | ForEach {
+    . $_.FullName
+    "... $( $_.FullName )"
+}
+<#
+# Load all exe files in subfolder
+$libExecutables = Get-ChildItem -Path ".\$( $libSubfolder )" -Recurse -Include @("*.exe") 
+$libExecutables | ForEach {
+    "... $( $_.FullName )"
+    
+}
+
+# Load dll files in subfolder
+$libExecutables = Get-ChildItem -Path ".\$( $libSubfolder )" -Recurse -Include @("*.dll") 
+$libExecutables | ForEach {
+    "Loading $( $_.FullName )"
+    [Reflection.Assembly]::LoadFile($_.FullName) 
+}
+#>
+
+
+################################################
+#
+# LOG INPUT PARAMETERS
+#
+################################################
+
+# Start the log
+Write-Log -message "----------------------------------------------------"
+Write-Log -message "$( $modulename )"
+Write-Log -message "Got a file with these arguments: $( [Environment]::GetCommandLineArgs() )"
+
+# Check if params object exists
+if (Get-Variable "params" -Scope Global -ErrorAction SilentlyContinue) {
+    $paramsExisting = $true
+} else {
+    $paramsExisting = $false
+}
+
+# Log the params, if existing
+if ( $paramsExisting ) {
+    $params.Keys | ForEach-Object {
+        $param = $_
+        Write-Log -message "$( $param ): $( $params[$param] )"
+    }
+}
+
+
+################################################
+#
+# PROGRAM
+#
+################################################
+
+
+
+
+#$settingsLoad = Invoke-RestMethod @params
+
+$stringSecure = ConvertTo-SecureString -String ( Get-SecureToPlaintext $settings.login.secret ) -AsPlainText -Force
+$cred = [pscredential]::new( $settings.login.username, $stringSecure )
+[Emarsys]::allowNewFieldCreation
+$emarsys = [Emarsys]::new($cred,$settings.base)
+$emarsys.getSettings()
+$f = $emarsys.getFields()
+$f | Out-GridView
+
+exit 0
+$emarsys.getEmailCampaigns()
+$emarsys.getAutomationCenterPrograms()
+$emarsys.getEmailTemplates()
+$emarsys.getExternalEvents()
+$emarsys.getLinkCategories()
+$emarsys.getLists()
+$emarsys.getSources()
+$emarsys.getAutoImportProfiles()
+$emarsys.getConditionalTextRules()
+
+
+#Invoke-emarsys -uri "$( $settings.base )settings" -cred $cred
+
+
+
+#-----------------------------------------------
+# FIELDS
+#-----------------------------------------------
+
+$url = "$( $settings.base )field/translate/de"
+Invoke-emarsys -uri "$( $settings.base )field/translate/de"
+
+
+$fields = Invoke-RestMethod -uri $url -Method Get -Headers $header -ContentType $contentType -Verbose
+$fields.data | Out-GridView
+
+exit 0
+
+#-----------------------------------------------
+# CAMPAIGNS
+#-----------------------------------------------
+
+$url = "https://trunk-int.s.emarsys.com/api/v2/email"
+$campaigns = Invoke-RestMethod -uri $url -Method Get -Headers $header -Verbose
+$selectedCampaigns = $campaigns.data | Out-GridView -PassThru
+
+
+#-----------------------------------------------
+# PREVIEW
+#-----------------------------------------------
+
+$selectedCampaigns | ForEach {
+    $id = $_.id 
+    $url = "https://trunk-int.s.emarsys.com/api/v2/email/$( $id )/preview"
+    $preview = Invoke-RestMethod -uri $url -Method Post -Headers $header -Verbose -Body "{""version"": ""html""}"
+    $preview.data
+}
+
+exit 0
+
+#-----------------------------------------------
+# LOAD TEMPLATES
+#-----------------------------------------------
+
+$templates = Get-eLetterTemplates
+
+
+#-----------------------------------------------
+# BUILD MAILING OBJECTS
+#-----------------------------------------------
+
+$mailings = @()
+$templates | foreach {
+
+    # Load data
+    $template = $_
+    #$id = Get-StringHash -inputString $template.url -hashName "MD5" #-uppercase
+
+    # Create mailing objects
+    $mailings += [Mailing]@{mailingId=$template.hashid;mailingName=$template.name}
+
+}
+
+$messages = $mailings | Select @{name="id";expression={ $_.mailingId }}, @{name="name";expression={ $_.toString() }}
+
+
+#-----------------------------------------------
+# GET MAILINGS DETAILS
+#-----------------------------------------------
+<#
+# The way without classes
+$messages = $result | where { $_.state -in $settings.mailings.states } | Select-Object @{name="id";expression={ $_.'_id' }},
+                                            @{name="name";expression={ "$( $_.'_id' )$( $settings.nameConcatChar )$( $_.name )"}} #$( if ($_.Description -ne '') { $settings.nameConcatChar } )$( $_.Description )" }}
+
+#>
+
+
+################################################
+#
+# RETURN
+#
+################################################
+
+# real messages
+return $messages
+
+
