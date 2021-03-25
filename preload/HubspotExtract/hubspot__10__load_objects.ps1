16a6685fb6e5e607223bb26bcc308e00318ff4b1
FlorianSf <florian.von.bracht@apteco.de>
2021-03-25 18:39:44 +0100

    Update hubspot__10__load_objects.ps1
    Allow to load the data directly into a sqlite database without csv as intermediate step. This way the raw json will be stored per item per row and can be parsed later when querying the data dynamically through designer.

--- preload/HubspotExtract/hubspot__10__load_objects.ps1
+++ preload/HubspotExtract/hubspot__10__load_objects.ps1
@@ -247,7 +247,7 @@ if (!($currentApiLimit -gt 0)) {
 # https://developers.hubspot.com/docs/api/crm/properties
 
 # TODO [ ] put this one into the settings
-$objectTypesToLoad = $settings.objectTypesToLoad
+$objectTypesToLoad = ( $settings.objectTypesToLoad | Get-Member -MemberType NoteProperty ).Name  #$settings.objectTypesToLoad
 
 $properties = [PSCustomObject]@{}
 $objectTypesToLoad | ForEach {
@@ -391,6 +391,7 @@ Switch ( $extractMethod ) {
 
             $limit = $settings.pageLimitGet
             $props = $properties.$objectType
+            $lastmodifiedProperty = $settings.objectTypesToLoad.$objectType.updatedField
 
             # Create body to ask for objects
             $body = [ordered]@{
@@ -398,14 +399,14 @@ Switch ( $extractMethod ) {
                     @{
                         "filters" = @(
                             @{
-                                "propertyName"="lastmodifieddate"
+                                "propertyName"=$lastmodifiedProperty
                                 "operator"="GTE"
                                 "value"= $lastTimestamp
                             }
                         )
                     }
                 )
-                sorts = @("lastmodifieddate")
+                sorts = @($lastmodifiedProperty)
                 #query = ""
                 properties = $props.name #@("firstname", "lastname", "email")
                 limit = $limit
@@ -424,10 +425,24 @@ Switch ( $extractMethod ) {
         
                 # Get all objects in page
                 $bodyJson = $body | ConvertTo-Json -Depth 8
-                $objRes = Invoke-RestMethod -Method Post -Uri $url -ContentType $contentType -Body $bodyJson -Verbose
+                try {
+
+                    $objRes = [PSCustomObject]@{}
+                    #$bodyJson
+                    $objRes = Invoke-RestMethod -Method Post -Uri $url -ContentType $contentType -Body $bodyJson -Verbose
+
+                    # Add objects to array
+                    $obj.AddRange( $objRes.results )
+
+                } catch {
+            
+                    $e = ParseErrorForResponseBody($_)
+                    Write-Log -message ( $e | ConvertTo-Json -Depth 20 -Compress ) -severity ([LogSeverity]::ERROR)
+                    throw $_.exception
+            
+                    #Write-Host $_ -fore green
+                }            
         
-                # Add objects to array
-                $obj.AddRange( $objRes.results )
     
                 Write-Log -message "Loaded $( $obj.count ) '$( $objectType )' in total"
                 
@@ -448,6 +463,309 @@ Switch ( $extractMethod ) {
 
 }
 
+#-----------------------------------------------
+# COUNT THE ITEMS TO SEE IF WE SHOULD PROCEED
+#-----------------------------------------------
+
+$itemsTotal = 0
+$objectTypesToLoad | ForEach {
+
+    $objectType = $_
+
+    $itemsTotal += $objects.$objectType.count
+
+}
+
+If ( $itemsTotal -gt 0 ) {
+    Write-Log -message "Counted $( $itemsTotal ) items in total -> Proceed loading into the database"
+} else {
+    Write-Log -message "No updated or new items -> Closing the process now"
+    exit 0
+}
+
+
+################################################
+#
+# BACKUP SQLITE FIRST
+#
+################################################
+
+Write-Log -message "Setting for creating backups $( $settings.backupSqlite )"
+
+if ( $settings.backupSqlite ) {  
+
+    # Create backup subfolder
+    $destination = $settings.backupDir
+    $destinationWithTimestamp = "$( $destination )\$( Get-Date $timestamp -Format "yyyyMMddHHmmss" )_$( $processId )\"
+    New-Item -Path $destinationWithTimestamp -ItemType Directory
+
+    Write-Log -message "Creating backup into $( $destinationWithTimestamp )"
+
+    # backup
+    $itemsToBackup | foreach {
+
+        $source = $_
+        
+        # Check if it is a file or folder
+        if ( Test-Path -Path $source -PathType Leaf ) {
+            # File
+        } else {
+            #Folder
+            $source = "$( $source )\*"
+        }
+
+        if ( Test-Path -Path $source ) {
+            Write-Log -message "Creating backup of $( $source )"    
+
+            Copy-Item -Path $source -Destination $destinationWithTimestamp -Force -Recurse
+
+        } else {
+
+            Write-Log -message "File $( $source ) does not exist -> no backup"
+
+        }
+
+    }
+
+}
+
+
+################################################
+#
+# LOAD DATA INTO SQLITE
+#
+################################################
+
+Write-Log -message "Loading data directly into sqlite"
+
+
+#-----------------------------------------------
+# ESTABLISHING CONNECTION TO SQLITE
+#-----------------------------------------------
+
+# Load assemblies for sqlite
+$assemblyFileSqlite = $libExecutables.Where({$_.name -eq "System.Data.SQLite.dll"})
+[Reflection.Assembly]::LoadFile($assemblyFileSqlite.FullName)
+
+# Create a new connection to a database (in-memory of file)
+# If the database does not exist, it will be created automatically
+$connection = [System.Data.SQLite.SQLiteConnection]::new()
+#$connection.ConnectionString = "Data Source=:memory:;Version=3;New=True;"
+$connection.ConnectionString = "Data Source=$( $settings.sqliteDb );Version=3;New=True;" # TODO [ ] put this into settings
+$connection.Open()
+
+# Load more extensions for sqlite, e.g. the Interop which includes json1
+#$connection.EnableExtensions($true)
+#$assemblyFileInterop = Get-Item -Path ".\sqlite-netFx46-binary-x64-2015-1.0.113.0\SQLite.Interop.dll"
+#$connection.LoadExtension($assemblyFileInterop.FullName, "sqlite3_json_init");
+
+# Create a new command which can be reused
+$command = [System.Data.SQLite.SQLiteCommand]::new($connection)
+
+
+#-----------------------------------------------
+# CREATE TABLES FOR STORING HUBSPOT CRM DATA
+#-----------------------------------------------
+
+<#
+# Drop a table, if exists
+$command.CommandText = "DROP TABLE IF EXISTS jobitems";
+[void]$command.ExecuteNonQuery();
+#>
+
+# Create a new table for object items, if it is not existing
+$command.CommandText = @"
+CREATE TABLE IF NOT EXISTS hubspot_items (
+     id TEXT
+    ,object TEXT 
+    ,ExtractTimestamp TEXT
+    ,createdAt TEXT
+    ,updatedAt TEXT
+    ,archived TEXT
+    ,properties TEXT
+)
+"@
+[void]$command.ExecuteNonQuery();
+
+# Create a new table for properties, if it is not existing
+$command.CommandText = @"
+CREATE TABLE IF NOT EXISTS hubspot_properties (
+     id TEXT
+    ,object TEXT 
+    ,ExtractTimestamp TEXT
+    ,createdAt TEXT
+    ,updatedAt TEXT
+    ,property TEXT
+)
+"@
+[void]$command.ExecuteNonQuery();
+
+
+#-----------------------------------------------
+# PREPARE QUERIES FOR INSERTING DATA
+#-----------------------------------------------
+
+# Prepare command for inserting properties
+$insertStatementProperties = @"
+INSERT INTO
+    hubspot_properties(id, object, ExtractTimestamp, createdAt, updatedAt, property)
+VALUES
+    (@id, @object, @ExtractTimestamp, @createdAt, @updatedAt, @property)
+"@
+
+# Prepare command for inserting rows
+$insertStatementItems = @"
+INSERT INTO
+    hubspot_items(id, object, ExtractTimestamp, createdAt, updatedAt, archived, properties)
+VALUES
+    (@id, @object, @ExtractTimestamp, @createdAt, @updatedAt, @archived, @properties)
+"@
+
+
+#-----------------------------------------------
+# LOADING DATA FROM HUBSPOT INTO SQLITE
+#-----------------------------------------------
+
+$insertedProperties = 0
+$insertedRows = 0
+$t = Measure-Command {
+
+    $objectTypesToLoad | ForEach {
+
+        $objectType = $_
+
+
+        #-----------------------------------------------
+        # LOADING PROPERTIES
+        #-----------------------------------------------
+
+        $properties.$objectType | ForEach {
+
+            $property = $_
+
+            $command.CommandText = $insertStatementProperties
+
+            [void]$command.Parameters.AddWithValue("@object", $objectType)        
+            [void]$command.Parameters.AddWithValue("@ExtractTimestamp", $currentTimestamp)
+
+            [void]$command.Parameters.AddWithValue("@id", $property.name)
+            [void]$command.Parameters.AddWithValue("@createdAt", $property.createdAt)
+            [void]$command.Parameters.AddWithValue("@updatedAt", $property.updatedAt)
+            [void]$command.Parameters.AddWithValue("@property", ( $property | convertto-json -Compress -Depth 20 ))
+
+            [void]$command.Prepare()
+
+            $insertedProperties += $command.ExecuteNonQuery()
+
+        }
+
+
+        #-----------------------------------------------
+        # LOADING ITEMS
+        #-----------------------------------------------
+
+        $objects.$objectType | ForEach {
+
+            $row = $_
+            
+            $command.CommandText = $insertStatementItems
+
+            [void]$command.Parameters.AddWithValue("@object", $objectType)        
+            [void]$command.Parameters.AddWithValue("@ExtractTimestamp", $currentTimestamp)
+
+            [void]$command.Parameters.AddWithValue("@id", $row.id)
+            [void]$command.Parameters.AddWithValue("@createdAt", $row.createdAt)
+            [void]$command.Parameters.AddWithValue("@updatedAt", $row.updatedAt)
+            [void]$command.Parameters.AddWithValue("@archived", $row.archived)
+            [void]$command.Parameters.AddWithValue("@properties", ( $row.properties | convertto-json -Compress -Depth 20 ))
+
+            [void]$command.Prepare()
+
+            $insertedRows += $command.ExecuteNonQuery()
+
+        }
+
+    }
+
+}
+
+
+#-----------------------------------------------
+# CLOSING CONNECTION
+#-----------------------------------------------
+
+$command.Dispose()
+$connection.Dispose()
+
+
+#-----------------------------------------------
+# LOG RESULTS
+#-----------------------------------------------
+
+Write-Log -message "Inserted $( $insertedProperties ) properties in total"
+Write-Log -message "Inserted $( $insertedRows ) items in total"
+Write-Log -message "Needed $( $t.TotalSeconds ) seconds for inserting data"
+
+
+################################################
+#
+# SAVE LAST LOADED TIMESTAMP
+#
+################################################
+
+$lastSession = @{
+    lastTimestamp = $currentTimestamp
+    lastTimeStampHuman = Get-Date $currentTimestampDateTime -Format "yyyyMMdd_HHmmss"
+}
+
+# create json object
+$lastSessionJson = $lastSession | ConvertTo-Json -Depth 8 # -compress
+
+# print settings to console
+$lastSessionJson
+
+# save settings to file
+$lastSessionJson | Set-Content -path "$( $settings.sessionFile )" -Encoding UTF8
+
+Write-Log -message "Saved the current timestamp '$( $currentTimestamp )' for the next run in '$( $scriptPath )\$( $lastSessionFilename )'"
+
+
+$objectTypesToLoad | ForEach {
+
+    $objectType = $_
+
+    # Exit if there is no new result
+    if ( $proceed -eq $false ) {
+        
+        Write-Log -message "No new data -> exit"
+        
+        Exit 0
+
+    }
+
+}
+
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ################################################
 #
@@ -462,6 +780,7 @@ Switch ( $extractMethod ) {
 $exportFolder = "$( $settings.exportDir )\$( $currentTimestamp )_$( $processId )"
 Write-Log -message "Folder $( $exportFolder ) does not exist. Creating the folder now!"
 New-Item -Path "$( $exportFolder )" -ItemType Directory
+
 
 #-----------------------------------------------
 # EXPORT FILES
@@ -507,85 +826,7 @@ $objectTypesToLoad | ForEach {
 
 
 
-################################################
-#
-# SAVE LAST LOADED TIMESTAMP
-#
-################################################
 
-$lastSession = @{
-    lastTimestamp = $currentTimestamp
-    lastTimeStampHuman = Get-Date $currentTimestampDateTime -Format "yyyyMMdd_HHmmss"
-}
-
-# create json object
-$lastSessionJson = $lastSession | ConvertTo-Json -Depth 8 # -compress
-
-# print settings to console
-$lastSessionJson
-
-# save settings to file
-$lastSessionJson | Set-Content -path "$( $settings.sessionFile )" -Encoding UTF8
-
-Write-Log -message "Saved the current timestamp '$( $currentTimestamp )' for the next run in '$( $scriptPath )\$( $lastSessionFilename )'"
-
-
-$objectTypesToLoad | ForEach {
-
-    $objectType = $_
-
-    # Exit if there is no new result
-    if ( $proceed -eq $false ) {
-        
-        Write-Log -message "No new data -> exit"
-        
-        Exit 0
-
-    }
-
-}
-
-
-
-################################################
-#
-# BACKUP SQLITE FIRST
-#
-################################################
-
-Write-Log -message "Setting for creating backups $( $settings.backupSqlite )"
-
-if ( $settings.backupSqlite ) {  
-
-    # Create backup subfolder
-    $destination = $settings.backupDir
-    $destinationWithTimestamp = "$( $destination )\$( Get-Date $timestamp -Format "yyyyMMddHHmmss" )_$( $processId )\"
-    New-Item -Path $destinationWithTimestamp -ItemType Directory
-
-    Write-Log -message "Creating backup into $( $destinationWithTimestamp )"
-
-    # backup
-    $itemsToBackup | foreach {
-
-        $source = $_
-        
-        # Check if it is a file or folder
-        if ( Test-Path -Path $source -PathType Leaf ) {
-            # File
-        } else {
-            #Folder
-            $source = "$( $source )\*"
-        }
-
-        if ( Test-Path -Path $source ) {
-            Write-Log -message "Creating backup of $( $source )"    
-
-            Copy-Item -Path $source -Destination $destinationWithTimestamp -Force -Recurse
-        }
-
-    }
-
-}
 
 
 ################################################
