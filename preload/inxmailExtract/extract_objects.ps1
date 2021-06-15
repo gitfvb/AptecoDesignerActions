8d33aa63256eb8c941033833483a3b438b665e7e
FlorianSf <florian.von.bracht@apteco.de>
2021-06-15 10:57:42 +0200

    First outlines for automatic #inxmail data extraction

--- preload/inxmailExtract/extract_objects.ps1
+++ preload/inxmailExtract/extract_objects.ps1
@@ -1,21 +1,479 @@
-[System.Collections.ArrayList]@(
+################################################
+#
+# INPUT
+#
+################################################
+
+Param(
+    [hashtable] $params
+)
+
+#-----------------------------------------------
+# DEBUG SWITCH
+#-----------------------------------------------
+
+$debug = $true
+
+#-----------------------------------------------
+# INPUT PARAMETERS, IF DEBUG IS TRUE
+#-----------------------------------------------
+
+if ( $debug ) {
+    $params = [hashtable]@{
+	    scriptPath= "D:\Scripts\Inxmail\Mailing"
+    }
+}
+
+
+################################################
+#
+# NOTES
+#
+################################################
+
+<#
+
+https://apidocs.inxmail.com/xpro/rest/v1/
+
+TODO [ ] implement paging
+
+#>
+
+################################################
+#
+# SCRIPT ROOT
+#
+################################################
+
+# if debug is on a local path by the person that is debugging will load
+# else it will use the param (input) path
+if ( $debug ) {
+    # Load scriptpath
+    if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+        $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+    } else {
+        $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+    }
+} else {
+    $scriptPath = "$( $params.scriptPath )" 
+}
+Set-Location -Path $scriptPath
+
+
+################################################
+#
+# SETTINGS
+#
+################################################
+
+# General settings
+$functionsSubfolder = "functions"
+#$libSubfolder = "lib"
+$settingsFilename = "settings.json"
+$moduleName = "INXRESPONSES"
+$processId = [guid]::NewGuid()
+
+# Load settings
+$settings = Get-Content -Path "$( $scriptPath )\$( $settingsFilename )" -Encoding UTF8 -Raw | ConvertFrom-Json
+
+# Allow only newer security protocols
+# hints: https://www.frankysweb.de/powershell-es-konnte-kein-geschuetzter-ssltls-kanal-erstellt-werden/
+if ( $settings.changeTLS ) {
+    $AllProtocols = @(    
+        [System.Net.SecurityProtocolType]::Tls12
+        #[System.Net.SecurityProtocolType]::Tls13,
+        #,[System.Net.SecurityProtocolType]::Ssl3
+    )
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+}
+
+# more settings
+$logfile = $settings.logfile
+$lastSessionFile = "$( $scriptPath )\lastsession.json"
+
+# append a suffix, if in debug mode
+if ( $debug ) {
+    $logfile = "$( $logfile ).debug"
+}
+
+
+################################################
+#
+# FUNCTIONS & ASSEMBLIES
+#
+################################################
+
+# Load all PowerShell Code
+"Loading..."
+Get-ChildItem -Path ".\$( $functionsSubfolder )" -Recurse -Include @("*.ps1") | ForEach-Object {
+    . $_.FullName
+    "... $( $_.FullName )"
+}
+
+
+################################################
+#
+# LOG INPUT PARAMETERS
+#
+################################################
+
+# Start the log
+Write-Log -message "----------------------------------------------------"
+Write-Log -message "$( $modulename )"
+Write-Log -message "Got a file with these arguments: $( [Environment]::GetCommandLineArgs() )"
+
+# Check if params object exists
+if (Get-Variable "params" -Scope Global -ErrorAction SilentlyContinue) {
+    $paramsExisting = $true
+} else {
+    $paramsExisting = $false
+}
+
+# Log the params, if existing
+if ( $paramsExisting ) {
+    $params.Keys | ForEach-Object {
+        $param = $_
+        Write-Log -message "    $( $param )= ""$( $params[$param] )"""
+    }
+}
+
+
+
+
+
+################################################
+#
+# PROGRAM
+#
+################################################
+
+
+
+#-----------------------------------------------
+# LOAD LAST EXTRACT
+#-----------------------------------------------
+
+If ( Test-Path -Path $lastSessionFile ) {
+    $startFromScratch = $false
+    $lastSession = Get-Content -Path "$( $lastSessionFile )" -Encoding UTF8 -Raw | ConvertFrom-Json
+} else {
+    $startFromScratch = $true
+}
+
+
+#-----------------------------------------------
+# MORE SETTINGs
+#-----------------------------------------------
+
+$extractTimestamp = Get-Unixtime 
+$earliestDate = "2021-01-10T00:00:00Z" # [Datetime]::UtcNow.ToString("yyyy-MM-ddTHH:mm:ssK")
+$extractMode = "full" # full|delta
+#$extractMode = "delta" # full|delta
+
+
+#-----------------------------------------------
+# AUTHENTICATION
+#-----------------------------------------------
+
+$apiRoot = $settings.base
+$contentType = "application/json; charset=utf-8"
+$auth = "$( Get-SecureToPlaintext -String $settings.login.authenticationHeader )"
+$header = @{
+    "Authorization" = $auth
+}
+
+
+#-----------------------------------------------
+# LOAD DEFINITION
+#-----------------------------------------------
+
+# TODO [ ] Eventually add a "first" load definition
+
+
+$loadDefs = [System.Collections.ArrayList]@(
+    
+    <#
+    
+    TODO [ ] create a template entry here
+    
+    #>
+
     [PSCustomObject]@{
+        "description" = "lists"
         "object" = "lists" #/lists{?createdAfter,createdBefore}
+        "urn" = "id"
+        "extract" = @(
+            [PSCustomObject]@{
+                "type" = "full"
+                "parameters" = [hashtable]@{
+                    "createdAfter" = $earliestDate
+                    #"pageSize" = 3
+                }
+                # do this to check if lists have been deleted or e.g. renamed
+            }
+            [PSCustomObject]@{
+                "type" = "delta"
+                "nextLink" = $lastSession.nextLinks.where( { $_.name -eq "lists"  } ).link
+                # remember the lastID URL to receive new lists, or timestamps to see what changed since then
+            }
+        ) #full|delta
+    }
+
+    [PSCustomObject]@{
+        "description" = "attributes"
+        "object" = "attributes" 
+        "urn" = "id"
+        "extract" = @(
+            [PSCustomObject]@{
+                "type" = "full"
+            }
+            [PSCustomObject]@{
+                "type" = "delta"
+            }
+        ) #full|delta
+    }
+<#
+    [PSCustomObject]@{
+        "description" = "sendings"
+        "object" = "sendings" #/sendings{?mailingIds,listIds,sendingsFinishedBeforeDate,sendingsFinishedAfterDate}
+        "urn" = "id"
+        "extract" = @(
+            [PSCustomObject]@{
+                "type" = "delta"
+                #"sendingsFinishedAfterDate" = "xxx"
+                # remember the lastID URL to receive new lists
+            }
+        ) #full|delta
+        "subObjects" = [System.Collections.ArrayList]@(
+            [PSCustomObject]@{
+                "description" = "sendingsprotocol"
+                "object" = [ScriptBlock]{
+                    "sendings/$( $parentObj.id )/protocol" # /sendings/{sendingId}/protocol
+                }
+                #"urn" = "id"
+                "extract" = @(
+                ) #full|delta
+            }
+        )
+    }
+#>
+<#
+    [PSCustomObject]@{
+        "object" = "recipients" #/recipients{?attributes,subscribedTo,lastModifiedSince,email,attributes.attributeName,trackingPermissionsForLists,subscriptionDatesForLists,unsubscriptionDatesForLists}
+        #"urn" = "id"
+        "extract" = @(
+            [PSCustomObject]@{
+                "type" = "full"
+                "attributes" = [ScriptBlock]{
+                    ( $inxArr | where { $_.object -eq "attributes"  } | ForEach { ConvertFrom-Json $_.payload  } | select name ).name -join ","
+                }
+                # no more parameters
+                # do this to check if lists have been deleted or e.g. renamed
+            }
+            [PSCustomObject]@{
+                "type" = "delta"
+                "createdAfter" = "xxx"
+                # remember the lastID URL to receive new lists
+            }
+        ) #full|delta
+    }
+#>
+
+
+        <#
+    [PSCustomObject]@{
+        "object" = "events/subscriptions" #/events/subscriptions{?listId,startDate,endDate,types,embedded,recipientAttributes}
+        # $ curl 'https://api.inxmail.com/customer/rest/v1/events/subscriptions?listId=5&startDate=2021-05-27T05:27:55Z&endDate=2021-05-27T07:27:55Z&types=PENDING_SUBSCRIPTION,PENDING_SUBSCRIPTION_DONE&embedded=inx:recipient&recipientAttributes=firstName,lastName' -i -X GET
         "extract" = @(
             "full" = [PSCustomObject]@{
                 # no more parameters
+                # do this to check if lists have been deleted or e.g. renamed
             }
             "delta" = [PSCustomObject]@{
                 "createdAfter" = "xxx"
+                # remember the lastID URL to receive new lists
             }
-
         ) #full|delta
     }
+
+
+
+    [PSCustomObject]@{
+        "object" = "recipients" #/recipients{?attributes,subscribedTo,lastModifiedSince,email,attributes.attributeName,trackingPermissionsForLists,subscriptionDatesForLists,unsubscriptionDatesForLists}
+        "extract" = @(
+            "full" = [PSCustomObject]@{
+                # no more parameters
+                # do this to check if lists have been deleted
+            }
+            "delta" = [PSCustomObject]@{
+                "attributes" = "" # comma separated list of attributes
+                "lastModifiedSince" = "xxx" # TODO [ ] does this also include new recipients?
+                # remember the lastID URL to receive new lists
+            }
+        ) #full|delta
+    }
+
+#>
+)
+
+
+
+#-----------------------------------------------
+# LOAD DATA
+#-----------------------------------------------
+
+function Get-Inxmail {
+    [CmdletBinding()]
+    param (
+         [Parameter(Mandatory=$true)][PSCustomObject] $definition
+        ,[Parameter(Mandatory=$true)][String] $extractMode
+        ,[Parameter(Mandatory=$false)][PSCustomObject] $parentObj = ""
+    )
+    
+    begin {
+        
+        $inxArr = [System.Collections.ArrayList]@()
+
+    }
+    
+    process {
+
+        $definition | where { $_.extract.type -eq $extractMode } | ForEach {
+    
+            $loadDef = $_
+            $extractSettings = $loadDef.extract.where({ $_.type -eq $extractMode })
+            
+            # Parse object url, if needed
+            If ( $loadDef.object -is [scriptblock] ) {
+                $objectUrl = $_.object.InvokeReturnAsIs()
+            } else {
+                $objectUrl = $loadDef.object
+            }
+
+            # Generate URI and additional query parameters
+            if ( $extractSettings.nextLink ) {
+                $uri = $extractSettings.nextLink
+            } else {
+                $uri = "$( $apiRoot )$( $objectUrl )"
+            }
+            if ( $extractSettings.parameters ) {
+                $uri = Add-HttpQueryPart -Uri $uri -QueryParameter $extractSettings.parameters
+            }
+
+            # Prepare http parameters
+            $params = [hashtable]@{
+                Method = "Get"
+                Uri = $uri
+                Header = $header
+                ContentType = "application/hal+json"
+                Verbose = $true
+            }
+
+            # Load data in pages
+            Do {
+
+                $res = Invoke-RestMethod @params
+            
+                # Parse the data
+                if ( $res._embedded ) {
+                    $urnFieldName = $loadDef.urn
+                    $firstProperty = ( $res._embedded | Get-Member -MemberType NoteProperty | select -first 1 ).Name
+                    $records = $res._embedded.$firstProperty  #$res._embedded."inx:$( $loadDef.object  )"
+                    $value =  $records | select @{name="object";expression={ $objectUrl }},
+                                                @{name="urn";expression={ $_.$urnFieldName }},
+                                                @{name="extract";expression={ $extractTimestamp }},
+                                                @{name="payload";expression={ ConvertTo-Json -InputObject $_ <# -Compress #> }}
+                    try {
+                        [void]$inxArr.AddRange(
+                            [System.Collections.ArrayList]@( $value )
+                        )
+                    } catch {
+                    #    [void]$inxArr.Add($value)
+                        "Hello world"
+                    }
+                }
+
+                $params.Uri = $res._links.next.href 
+
+            } While ( $res._links.next )
+
+            # Link for next time
+            $nextLink = $res._links."inx:upcoming".href
+            if ( $nextLink ) {
+                [void]$script:nextLinks.Add([PSCustomObject]@{
+                    name = $objectUrl
+                    link = $nextLink
+                })
+            }
+        
+            # Go into subobjects, if defined, maybe recursive
+            if ( $loadDef.subObjects ) {
+                $records | ForEach {
+                    $record = $_
+                    $subRes = Get-Inxmail -definition $loadDef.subObjects -parentObj $record -extractMode $extractMode
+                    try {
+                    [void]$inxArr.AddRange( 
+                        [System.Collections.ArrayList]@( $subRes )
+                    )
+                    } catch {
+                        "Hallo Welt"
+                    }
+                }
+            }
+        }
+
+    }
+    
+    end {
+        
+        $inxArr
+
+    }
+
+}
+
+$nextLinks = [System.Collections.ArrayList]@()
+$inxObjects = [System.Collections.ArrayList]@()
+$inxObjects.AddRange(( Get-Inxmail -definition $loadDefs -extractMode $extractMode ))
+
+$inxObjects | Out-GridView
+
+
+################################################
+#
+# PACK TOGETHER RESULTS AND SAVE AS JSON
+#
+################################################
+
+$session = [PSCustomObject]@{
+    timestamp = $extractTimestamp
+    nextLinks = $nextLinks
+}
+
+# create json object
+# weil json-Dateien sind sehr einfach portabel
+$json = $session | ConvertTo-Json -Depth 20 # -compress
+
+# print settings to console
+$json
+
+# save settings to file
+$json | Set-Content -path $lastSessionFile -Encoding UTF8
+
+
+
+
+
+
+exit 0
+
+
+
     <#
 
     https://apidocs.inxmail.com/xpro/rest/v1/
 
-    list settings not available to read on 2021-06-11
+    x /list settings not available to read on 2021-06-11
     /mailings{?createdAfter,createdBefore,modifiedAfter,modifiedBefore,sentAfter,types,listIds,readyToSend,embedded}
     /regular-mailings{?createdAfter,createdBefore,modifiedAfter,modifiedBefore,sentAfter,sentBefore,types,listIds,readyToSend,mailingStates,embedded},
     /split-test-mailings{?createdAfter,createdBefore,modifiedAfter,modifiedBefore,sentAfter,listIds,readyToSend}
@@ -84,5 +542,42 @@
     next            The link relation for the immediate next page of results.
     inx:upcoming    Links to a possible next page. This next page is only available once further data has been created in the system.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $ curl 'https://api.inxmail.com/customer/rest/v1/recipients?lastModifiedSince=2018-01-16T11:42:32Z' -i -X GET
     #>
-)
---
+++ preload/inxmailExtract/functions/http/Prepare-MultipartUpload.ps1
@@ -0,0 +1,91 @@
+<#
+
+# Do it with a file like a picture
+$mp = Prepare-MultipartUpload -path "image.jpg"
+
+# Do it directly with text information
+$csv = import-csv "test.csv" -Encoding UTF8 -Delimiter ";"
+$csvString = $csv | ConvertTo-Csv -Delimiter ";" -NoTypeInformation
+$mp = Prepare-MultipartUpload -string $csvString
+
+
+#>
+function Prepare-MultipartUpload {
+    [CmdletBinding()]
+
+    param(
+        [Parameter(Mandatory=$false)][String]$path = "",
+        [Parameter(Mandatory=$false)][String[]]$string = @(""),
+        [Parameter(Mandatory=$false)]$part = $false
+    )
+    
+    begin {
+        
+        if ( $path -eq "" -and $string -eq "" ) {
+            throw [System.IO.InvalidDataException] "You must define either path or string. Both is empty"
+        }
+
+        # standard settings
+        $uploadEncoding = "ISO-8859-1" #"UTF-8"
+        $crlf = "`r`n";
+
+        # Read a file()
+        if ( $path -ne "" ) {
+            # if multipart, remove the part prefix
+            $fileItem = Get-Item -Path $path
+            if ($part) {
+                $fileName = $fileItem.Name.Substring(0, $fileItem.Name.lastIndexOf('.')) 
+            } else {
+                $fileName = $fileItem.Name
+            }
+
+            # get file, load and encode it
+            $fileBytes = [System.IO.File]::ReadAllBytes($fileItem.FullName)
+
+        }
+
+        # Use a string
+        if ( $string -ne "" ) {
+            $oneString = $string -join $crlf
+            $fileBytes = [Text.Encoding]::UTF8.GetBytes( $oneString )
+            $filename = "$( [System.Guid]::NewGuid() ).txt"
+        }
+
+        # Encode the string
+        $fileEncoded = [System.Text.Encoding]::GetEncoding($uploadEncoding).GetString($fileBytes)
+
+    }
+    
+    process {
+        
+
+
+        # create guid for multipart upload
+        $boundary = [System.Guid]::NewGuid().ToString().replace("-","").PadLeft(57,"-")
+
+        # Get the mime type
+        $mimeType = [System.Web.MimeMapping]::GetMimeMapping($fileName)
+
+        # create body
+        $body = (
+            #"multipart/form-data; boundary=$( $boundary )",
+            "--$( $boundary )",
+            "Content-Disposition: form-data; name=`"file`"; filename=`"$( $fileName )`"",
+            "Content-Type: $( $mimeType )$( $crlf )",
+            #"Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet$( $crlf )",
+            $fileEncoded,
+            "--$( $boundary )--$( $crlf )" 
+        ) -join $crlf
+
+    }
+    
+    end {
+        
+        # put it together
+        @{
+            "body"=$body
+            "contentType"="multipart/form-data; boundary=$( $boundary )"
+        }
+
+    }
+}
