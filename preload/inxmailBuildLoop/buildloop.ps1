60f4e93613bdeb7c358b104f5caa18c19beddbca
FlorianSf <florian.von.bracht@apteco.de>
2021-06-18 17:24:26 +0200

    Create buildloop.ps1
    Adding a script to load the #inxmail data, then wait a few seconds and wait for the next build.now file generated

---
+++ preload/inxmailBuildLoop/buildloop.ps1
@@ -0,0 +1,9 @@
+﻿# execute designer console
+
+Do {
+
+    & DesignerConsole.exe "D:\FastStats\Build\Inxmail\designs\inxmail.xml" /load     
+
+    Start-Sleep -Seconds 120 #300
+
+} while (1 -eq 1)
